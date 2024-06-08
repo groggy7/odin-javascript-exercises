756acd85c41c3882800e058ff679be89ac924ce1
Serhat Kılbaş <serhat.kilbas@priviasecurity.com>
2024-06-08 19:57:30 +0300

    Fix solution to pass other test cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
 const repeatString = function(word, num) {
-    return word.repeat(num);
+    if(num >= 0) {
+        return word.repeat(num);
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
