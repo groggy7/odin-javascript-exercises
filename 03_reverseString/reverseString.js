551a075c39a96d6743575067dc6e3cf4d64941a3
Serhat Kılbaş <serhat.kilbas@priviasecurity.com>
2024-06-08 20:05:26 +0300

    Add solution for exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    const lastCharIndex = word.length - 1;
 
+    let reversedString = ""
+    for(let i = lastCharIndex; i >= 0; i--) {
+        reversedString += word[i];
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
