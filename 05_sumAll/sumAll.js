ad7ad1729603fbb931f856b7e03eea536e5b9e8a
Serhat Kılbaş <serhat.kilbas@priviasecurity.com>
2024-06-09 11:53:36 +0300

    Improve solution for exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,28 +1,20 @@
-const sumAll = function(...nums) {
+const sumAll = function(num1, num2) {
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    if (num1 < 0 || num2 < 0) return "ERROR";
+
+    if(num1 > num2) {
+        const temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
     let sum = 0;
-
-    for (const elem of nums) {
-        if (typeof elem !== 'number' || Number.isNaN(elem)) {
-            return "ERROR"
-        }       
-    }
-    if (nums[0] < 0 || nums[1] < 0) {
-        return "ERROR"
-    }
-
-    if(nums[0] > nums[1]) {
-        let temp = nums[0];
-        nums[0] = nums[1];
-        nums[1] = temp;
-    }
-
-    for(let i = nums[0]; i <= nums[1]; i++) {
+    for(let i = num1; i <= num2; i++) {
         sum += i;
     }
 
     return sum;
 };
 
-sumAll(1, 4)
 // Do not edit below this line
 module.exports = sumAll;
