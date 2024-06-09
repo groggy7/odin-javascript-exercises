040d939b0e53fba3980598447f6333b850d9ec09
Serhat Kılbaş <serhat.kilbas@priviasecurity.com>
2024-06-09 11:12:58 +0300

    Add solution for exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...nums) {
+    let newArray = [];
+
+    for(let i = 0; i < array.length; i++) {
+        if(!nums.includes(array[i])) {
+            newArray.push(array[i]);
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
