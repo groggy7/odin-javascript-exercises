1e3995d5671531ba0591bee79b3e802dd673130a
Serhat Kılbaş <serhat.kilbas@priviasecurity.com>
2024-06-11 21:42:24 +0300

    Add solution for exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1+num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1-num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0
+
+  array.forEach(element => {
+    sum += element  
+  })
+
+  return sum
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let result = 1
 
+  array.forEach(element => {
+    result *= element
+  });
+
+  return result
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return num ** power
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num === 0 || num === 1) {
+    return 1
+  }
+
+  let result = 1
+  for(let i = num; i > 1; i--) {
+    result *= i
+  }
+
+  return result
 };
 
 // Do not edit below this line
