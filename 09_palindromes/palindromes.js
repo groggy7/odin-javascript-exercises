bbbff4d1e398dcb86f9ee9324da93ccad6866545
Serhat Kılbaş <serhat.kilbas@priviasecurity.com>
2024-06-11 22:22:30 +0300

    Add solution for exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const regex = /[^\ws]/g;
+    string = string.toLowerCase()
+    string = string.replace(regex, "")
 
+    let reversedString = ""
+    for(let i = string.length - 1; i >= 0; i--) {
+        reversedString += string[i]
+    }
+
+    return string === reversedString
 };
 
+console.log(palindromes("racecar"))
 // Do not edit below this line
 module.exports = palindromes;
