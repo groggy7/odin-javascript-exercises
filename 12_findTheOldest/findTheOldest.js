459c61b2e6417943d34995073d67248a190074e7
Serhat Kılbaş <serhat.kilbas@priviasecurity.com>
2024-06-13 21:53:24 +0300

    Add solution for exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const currentYear = new Date().getFullYear()
+    return array.reduce((acc, curr) => {
+        const ageOfPerson = (curr.yearOfDeath || currentYear) - curr.yearOfBirth
+        const biggestAge = (acc.yearOfDeath || currentYear) - acc.yearOfBirth
 
+        if (ageOfPerson > biggestAge) {
+            return curr
+        }
+        return acc
+    }, array[0])
 };
+
+
+// another solution
+/*
+const findTheOldest = function(array) {
+    let biggestAge = 0
+    return array.reduce((acc, curr) => {
+        const ageOfPerson = (curr.yearOfDeath || new Date().getFullYear()) - curr.yearOfBirth
+        
+        if (ageOfPerson > biggestAge) {
+            biggestAge = ageOfPerson
+            return curr
+        }
+    }, array[0])
+};
+*/
 
 // Do not edit below this line
 module.exports = findTheOldest;
